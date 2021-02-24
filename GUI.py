c81700a42f1a59fc7d57442ab706261ec4a3ffd0
DragonTNT1112 <39580314+DragonTNT1112@users.noreply.github.com>
2021-02-24 22:08:59 +1300

    GUI Updated

--- GUI.py
+++ GUI.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
 import numpy as np
+from datetime import datetime
+import os
 
 window_width = 1600
 window_height = 1000
@@ -21,12 +23,19 @@ Font_thickness = 2
 
 
 class MainWindow():
-    def __init__(self, window, cap):
+    def __init__(self, window, cap, Pod_ID):
+        self.ID = Pod_ID
         self.window = window
 
         self.Pod_Status = None
         self.Obj_Status = None
         self.Mot_Status = None
+
+        self.check_in_time = datetime.now()
+        self.check_out_time = datetime.now()
+
+        self.in_switch = False
+        self.out_switch = False
 
         self.cap = cap
         self.interval = int(1000/FPS)  # Interval in ms to get the latest frame
@@ -34,14 +43,14 @@ class MainWindow():
         # Create canvas for image
         self.set_up_window(window)
 
-        self.get_base_image()
         self.real_frame = None
         # Update image on canvas
+        self.get_base_image()
         self.update_image()
 
     def get_base_image(self):
-        self.base_image = self.cap.read()[1]
-        self.base_image = cv2.cvtColor(self.base_image, cv2.COLOR_BGR2GRAY)
+        self.base_image_unchanged = self.cap.read()[1]
+        self.base_image = cv2.cvtColor(self.base_image_unchanged, cv2.COLOR_BGR2GRAY)
         self.base_image = cv2.GaussianBlur(self.base_image, (25, 25), 0)
 
     def convert2tk(self, img):
@@ -53,6 +62,48 @@ class MainWindow():
     def get_current_frame(self, real_frame):
         self.current_frame = cv2.cvtColor(real_frame, cv2.COLOR_BGR2GRAY)
         self.current_frame = cv2.GaussianBlur(self.current_frame, (25, 25), 0)
+
+    def CheckInCallBack(self):
+        self.in_switch = True
+        self.check_in_time = datetime.now()
+        self.get_base_image()
+        print("Start to check in...")
+
+    def CheckOutCallBack(self):
+        self.out_switch = True
+        self.check_out_time = datetime.now()
+        print("Start to check out...")
+
+    def capture_image(self):
+        current_time = datetime.now()
+        date_folder = str(current_time.date())
+        if current_time.minute < 30:
+            Hour = str(current_time.hour)
+            next_hour = Hour
+            Min = '00'
+            next_min = '30'
+        else:
+            Hour = str(current_time.hour)
+            next_hour = str(int(current_time.hour) + 1)
+            Min = '30'
+            next_min = '00'
+
+        time_period = "{}_{} - {}_{}".format(Hour, Min, next_hour, next_min)
+        file_name = str(current_time.time())[:-7].replace(":", "_")
+
+        format = ".jpg"
+        path = r"C:\Users\zzhu827\PycharmProjects\Booqed-updated\Check_in_out_images\{}\{}\{}".format(self.ID,
+                                                                                                  date_folder, time_period)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        directory = "Check_in_out_images/{}/{}/{}/".format(self.ID, date_folder, time_period)
+        cv2.imwrite(directory + file_name + format, self.real_frame)
+
+        if self.Obj_Status == True:
+            file_name = str(current_time.time())[:-7].replace(":", "_") + "_Reference"
+            cv2.imwrite(directory + file_name + format, self.base_image_unchanged)
 
     def get_object(self):
         # Calculating the difference and image thresholding
@@ -96,6 +147,16 @@ class MainWindow():
 
         # Drawing rectangles bounding the contours
         if len(contours) > 0:
+            areas = [cv2.contourArea(contour) for contour in contours]
+            max_index = np.argmax(areas)
+            cnt = contours[max_index]
+
+            if cv2.contourArea(cnt) < 200:
+                pass
+            else:
+                (x, y, w, h) = cv2.boundingRect(cnt)
+                cv2.rectangle(self.real_frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
             self.Mot_Status = True
             self.Mot_txt = "Motion Detected"
             self.Mot_txt_color = "red"
@@ -105,6 +166,7 @@ class MainWindow():
             self.Mot_txt_color = "green"
 
     def get_pod_status(self):
+
         if self.Mot_Status and self.Obj_Status:
             self.Pod_Status = "Occupied"
             self.Pod_txt_color = "red"
@@ -112,6 +174,28 @@ class MainWindow():
         if (not self.Mot_Status) and (not self.Obj_Status):
             self.Pod_Status = "Empty"
             self.Pod_txt_color = "Green"
+
+        if self.Obj_Status == True and self.out_switch == True:
+            self.Pod_Status == "Occupied"
+            self.Pod_txt_color = "red"
+
+        if self.Mot_Status:
+            self.check_out_time = datetime.now()
+
+        if not self.Obj_Status:
+            self.check_in_time = datetime.now()
+
+        current_time = datetime.now()
+        in_difference = current_time - self.check_in_time
+        out_difference = current_time - self.check_out_time
+
+        if in_difference.seconds >=15 and self.in_switch == True:
+            self.in_switch = False
+            print("User has checked in!")
+
+        if out_difference.seconds >= 15 and self.out_switch == True:
+            self.out_switch = False
+            print("User has checked out!")
 
     def update_image(self):
         # Get the latest frame and convert image format
@@ -138,34 +222,54 @@ class MainWindow():
         self.window.after(self.interval, self.update_image)
 
     def set_up_window(self, window):
+
         window.title("Qubic Management")
         # window.geometry("{}x{}".format(window_width, window_height))
 
-        l1 = tk.Label(window, text="Qubic Status", font=("Helvetica 16 bold"), width=20, anchor="w")
-        l2 = tk.Label(window, text="Object Detection", font=("Helvetica 16 bold"), width=20, anchor="w")
-        l3 = tk.Label(window, text="Motion Detection", font=("Helvetica 16 bold"), width=20, anchor="w")
+        title = "POD ID - {}".format(self.ID)
+        title_ID = tk.Label(window, text=title, font=("Helvetica 20 bold"), bg="#42a4f5", fg="white", height=2, width=30)
+        title_ID.grid(row=0,column=1, columnspan=2, padx=5, pady=5)
 
-        l1.grid(row=0, column=1, padx=5, pady=5)
-        l2.grid(row=3, column=1, padx=5, pady=5)
-        l3.grid(row=6, column=1, padx=5, pady=5)
+        # Labels for the Pod Status
+        l1 = tk.Label(window, text="Qubic Status:", font=("Helvetica 16 bold"), width=15, anchor="w")
+        l2 = tk.Label(window, text="Object Detection:", font=("Helvetica 16 bold"), width=15, anchor="w")
+        l3 = tk.Label(window, text="Motion Detection:", font=("Helvetica 16 bold"), width=15, anchor="w")
 
-        self.Pod = tk.Label(window, text=self.Pod_Status, font=("Helvetica 16 bold"), width=20, anchor="w")
-        self.Pod.grid(row=1, column=1, padx=5, pady=5)
+        l1.grid(row=1, column=1, stick="w")
+        l2.grid(row=3, column=1, stick="w")
+        l3.grid(row=4, column=1, stick="w")
 
-        self.Obj = tk.Label(window, text=self.Obj_Status, font=("Helvetica 16 bold"), width=20, anchor="w")
-        self.Obj.grid(row=4, column=1, padx=5, pady=5)
+        self.Pod = tk.Label(window, text=self.Pod_Status, font=("Helvetica 16 bold"), width=15, anchor="w")
+        self.Pod.grid(row=1, column=2, stick="w")
 
-        self.Mot = tk.Label(window, text=self.Mot_Status, font=("Helvetica 16 bold"), width=20, anchor="w")
-        self.Mot.grid(row=7, column=1, padx=5, pady=5)
+        self.Obj = tk.Label(window, text=self.Obj_Status, font=("Helvetica 16 bold"), width=15, anchor="w")
+        self.Obj.grid(row=3, column=2, stick="w")
 
+        self.Mot = tk.Label(window, text=self.Mot_Status, font=("Helvetica 16 bold"), width=15, anchor="w")
+        self.Mot.grid(row=4, column=2, stick="w")
+
+        # Buttons
+        check_in = tk.Button(window, text="Start to Check In", command=self.CheckInCallBack,
+                              font=("Helvetica 16 bold"), width=15)
+        check_in.grid(row=6, column=1, padx=5, pady=5, stick="w")
+
+        check_out = tk.Button(window, text="Start to Check Out", command=self.CheckOutCallBack,
+                              font=("Helvetica 16 bold"), width=15)
+        check_out.grid(row=7, column=1, padx=5, pady=5, stick="w")
+
+        take_img = tk.Button(window, text="Capture Image", command=self.capture_image,
+                              font=("Helvetica 16 bold"), width=15)
+        take_img.grid(row=8, column=1, padx=5, pady=5, stick="w")
+
+        # Image Canvas
         self.canvas_ori = tk.Canvas(self.window, width=camera_width, height=camera_height)
-        self.canvas_ori.grid(row=0, column=0, rowspan=9, padx=5, pady=5)
+        self.canvas_ori.grid(row=0, column=0, rowspan=12, padx=5, pady=5)
 
         self.canvas_capture = tk.Canvas(self.window, width=camera_width, height=camera_height)
-        self.canvas_capture.grid(row=9, column=0, rowspan=3, padx=5, pady=5)
+        self.canvas_capture.grid(row=12, column=0, rowspan=12, padx=5, pady=5)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    main_window = MainWindow(root, cv2.VideoCapture(0))
+    main_window = MainWindow(root, cv2.VideoCapture(0), 255553)
     root.mainloop()
